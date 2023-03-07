a5e4f8dff768ec4117eeceb112e43ec6999efa10
NachoPolinari <nachopolinari@gmail.com>
2023-03-07 00:33:46 -0300

    homewort06

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -2,121 +2,129 @@
 
 /* EJERCICIO 1
 Implementar la clase LinkedList, definiendo los siguientes métodos:
-  - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular 
-    de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: 
-  el parámetro puede ser 
-  un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; 
-  en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
-  EJEMPLO 
-  search(3) busca un nodo cuyo valor sea 3;
-  search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, 
-  busca un nodo cuyo valor sea un número par.
-  En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 function LinkedList() {
   /* 1 - se intancia el head*/
   this.head = null
-  //this.size= 0
-  //this.tail= null
 }
 
 function Node(value) {
   /* 2 - se instancia value*/
-  this.value = value;
+  this.value = value
   /* 3 - se instancia next*/
-  this.next = null;
-
-
+  this.next = null
 }
+
+//- add: agrega un nuevo nodo al final de la lista;
 //--FUNCION ADD-->
 
 /* 4 - le agrego la funcion add a linkedlist*/
-LinkedList.prototype.add = function (value) {
+LinkedList.prototype.add = function () {
+
   /* 5 - instanciar un nodo con ese valor*/
-  var newNode = new Node(value);
+  var newNode = new Node(value)
   /* 6 - crear current ->es un apuntador auxiliar (busca y apunta al ultimo nodo) */
   var current = this.head
   /* 7 - SI la lista estaba vacia -> newNode pasa a ser this.head, es decir current */
-  if (!current /*current===null*/ ) {
+  if (!current) {
     this.head = newNode
-    return value;
+    return value
   } else {
-
     /* 8 - SI la lista NO estaba vacia -> comienza el recorrido*/
-    while (current.next /*current.next !== null*/ ) {
+    while (current.next)
       /* 9 - agregar newNode al final de la lista */
-      current = current.next;
-    }
-    current.next = newNode
-    return value;
+      current = current.next
   }
+  /* 10 - cuando descubre que no hay current.next sale del while, crea el nuevo nodo y retorna value */
+  current.next = newNode
+  return value;
 }
 
-var myList = new LinkedList()
-myList.add(5)
-myList.add(10)
-myList.add(15)
-myList.add(20)
-myList.add(25)
-console.log(myList);
-
+//- remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular 
+// de una lista de un solo nodo y de una lista vacía);
 // --FUNCION REMOVE--->
+
+/* *while(curren.next)=> A dnd llego? Termino parado en el ultimo nodo de la lista */
+/* *while(curren.next.next)=> A dnd llego? Termino parado en el ante-ultimo nodo de la lista */
+/* *while(curren)=>a dnd llego? Termino fuera de la lista - Me caigo*/
+
 LinkedList.prototype.remove = function () {
-  /* *while(curren.next)=> A dnd llego? Termino parado en el ultimo nodo de la lista */
-  /* *while(curren.next.next)=> A dnd llego? Termino parado en el ante-ultimo nodo de la lista */
-  /* *while(curren)=>a dnd llego? Termino fuera de la lista - Me caigo*/
-  //NO TENGO NODO =>NADA QUE HACER
-  //TENGO UN SOLO NODO =>NO TENGO NINGUN ANTEULTIMO
-  //TENGO MAS DE UN NODO=>VOY HASTA EL ANTEULTIMO Y ELIMINO AL PROXIMO
   var current = this.head
   /*LALISTA ESTABA VACIA */
-  if (!current) return null;
+  //NO TENGO NODO =>NADA QUE HACER
+  if (!current) {
+    return null
+  }
   /*LA LISTA TENIA UN SOLO NODO */
+  //TENGO UN SOLO NODO =>NO TENGO NINGUN ANTEULTIMO
   else if (!current.next) {
-    var aux = current.value;
-    this.head = null;
-    return aux
+    var aux = current.value
+    this.head = null
+    return aux;
   }
   /*LA LISTA TENIA VARIOS NODOS */
+  //TENGO MAS DE UN NODO=>VOY HASTA EL ANTEULTIMO Y ELIMINO AL PROXIMO
   while (current.next.next) {
+
     current = current.next
+    var aux = current.next.value
+    current.next = null
+    return aux
   }
-  var aux = current.next.value
-  current.next = null
-  return aux
+
 }
-
-myList.remove()
-myList.remove()
-
-console.log(myList);
+// - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: 
+// el parámetro puede ser 
+// un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; 
+// en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+// EJEMPLO 
+// search(3) busca un nodo cuyo valor sea 3;
+// search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, 
+// busca un nodo cuyo valor sea un número par.
+// En caso de que la búsqueda no arroje resultados, search debe retornar null.
 
 //--FUNCION SEARCH---->
-LinkedList.prototype.search = function (param /* :value o function */ ) {
+LinkedList.prototype.search = function (param /*value o cb */ ) {
+
   //SI VALUE ES UN VALOR=> BUSCAR VALOR
   //SI VALUE ES UNA CALLBACK=>BUSCAR NODO QUE COINCIDA CON ESE VALOR Y QUE RETORNE TRUE - EJEMPLO isEven(busca numeros pares)
   var current = this.head;
-  if (!current) return false;
+  if (!current) return false; /* esto lo saco de los tests */
 
-  if (typeof param === "function") {//tengo una funcion
-    while (current) {/*mientras haya algun current.. */
-    if (param(current.value)) return current.value ;/*si current.value es igual a param retornamelo */
-    else current = current.next;/*sino que current sea current.next para pasar al sig nodo */
-  }
-  return null
-  } else {
-    //tengo otra cosa
-    while (current) {/*mientras haya algun current.. */
-      if (current.value === param) return param /*si current.value es igual a param retornamelo */
-      else current = current.next;/*sino que current sea current.next para pasar al sig nodo */
+  //si tengo una funcion
+  if (typeof param === "function") {
+
+    /*mientras haya algun current.. */
+    while (current) {
+      /*si current.value es igual a param retornamelo */
+      if (param(current.value)) {
+        return current.value
+        /*sino que current sea current.next para pasar al sig nodo */
+      } else {
+        current = current.next
+      }
+      return null
     }
-    return null /*si recorri todos los nodos y no encontre un value igual, retorna null */
+    //si tengo otra cosa
+  } else {
+
+
+    /*mientras haya algun current.. */
+    while (current) {
+      /*si current.value es igual a param retornamelo */
+      if (current.value === param) {
+        return param;
+        /*sino que current sea current.next para pasar al sig nodo */
+      } else {
+        current = current.next
+      }
+      /*si recorri todos los nodos y no encontre un value igual, retorna null */
+      return null
+
+    }
+
   }
-  //No tengo nodos
-  //tengo un solo nodo
-  // Tengo varios nodos
+
 }
 
 //---------------------------------------------------------
@@ -146,50 +154,60 @@ si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('in
 se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 function HashTable() {
-  this.numBuckets = 35
-  this.buckets = []
+  //creo numBuckets y buckets
+  this.buckets= []
+this.numBuckets = 35
 }
 
-HashTable.prototype.hash = function(key){ //--------------------------------/*instancia la funcion hash para la clase HasTable */
-  //JORGE
-  //
-  var suma = 0 /*un acumulador para el total */
-  for(var char of key){ /*recorro la key(array) con for of y nombro a cada bucket var=char*/
-  //TAMBIEN se puede hacer con un for => for (let i=0 ; i<key.lenght, i++){
-  //                                      var char= array[i]}
-    suma += char.charCodeAt() /*la accion que ejecuta el for of es convertir cada letra en un numero Unicode y sumarlo(ver hash y charCodeaAt)  */
-  }
-  return suma % this.numBuckets //esto es el indice del array donde lo voy a guardar */
-}
+//--------------------------------/*instancia la funcion hash para la clase HasTable */
+//JORGE
+//
+/*un acumulador para el total */
 
-HashTable.prototype.set = function(key,value){//-----------------------------------/*instancia la funcion set para la clase HashTable */
-if (typeof key !=="string") throw TypeError("key must be strings"); /* deberia devolver un error cuando el key no es un string */
-  var index = this.hash(key); // hasheo la key y la guardo en index */
+/*recorro la key(array) con for of y nombro a cada bucket var=char*/
 
-if (!this.buckets[index]){ // si el bucket señalado por el index esta vacio, */
-    this.buckets[index] = {[key]:value}; //guardo el obj{key:value} en el index señalado
-}else{
-  this.buckets[index][key] = value; /* si no esta vacio, le asigno un nuevo valor a la key de ese bucket */
-}
-}
+//TAMBIEN se puede hacer con un for => for (let i=0 ; i<key.lenght, i++){
+//                                      var char= array[i]}
+/*la accion que ejecuta el for of es convertir cada letra en un numero Unicode y sumarlo(ver hash y charCodeaAt)  */
 
-HashTable.prototype.get = function(key){ //------------------------------------/* instancia la funcion get para la clase HashTable */
-  var index = this.hash(key) /*hasheo la key y la guardo en index */
-  return this.buckets[index][key] /*guardo el valor de key en el bucket indicado por index y retorno */
-}
+//esto es el indice del array donde lo voy a guardar */
 
-HashTable.prototype.hasKey = function(key){//----------------------------------/*intancio la funcion hasKey para la clase HashTable */
-  var index = this.hash(key); /*hasheo la key y la guardo en index */
-    if(this.buckets[index][key]){ /*si hay un bucket en este index con esta key */
-      return true  /*retorname true */
-    }else{
-      return false; /*sino retorname false */
-    }
 
-    //OTRAS FORMAS DE EXPRESAR LO MISMO:
-    //return !! this.buckets[index][key] --------- la doble negacion retorna booleano->existe este buckets con esta key en este index ? T  F
-    //  return !!this.get(key) -------la otra forma es usar el metodo ya creado, pasarle la key y buscar booleano por doble negacion.
-}
+//-----------------------------------/*instancia la funcion set para la clase HashTable */
+/*if (typeof key !=="string") throw TypeError("key must be strings");*/
+/* deberia devolver un error cuando el key no es un string */
+
+// hasheo la key y la guardo en index */
+
+// si el bucket señalado por el index esta vacio, */
+
+//guardo el obj{key:value} en el index señalado
+
+/* si no esta vacio, le asigno un nuevo valor a la key de ese bucket */
+
+
+//------------------------------------/* instancia la funcion get para la clase HashTable */
+
+/*hasheo la key y la guardo en index */
+
+/*guardo el valor de key en el bucket indicado por index y retorno */
+
+
+//----------------------------------/*intancio la funcion hasKey para la clase HashTable */
+
+/*hasheo la key y la guardo en index */
+
+/*si hay un bucket en este index con esta key */
+
+/*retorname true */
+
+/*sino retorname false */
+
+
+//OTRAS FORMAS DE EXPRESAR LO MISMO:
+//return !! this.buckets[index][key] --------- la doble negacion retorna booleano->existe este buckets con esta key en este index ? T  F
+//  return !!this.get(key) -------la otra forma es usar el metodo ya creado, pasarle la key y buscar booleano por doble negacion.
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -5,11 +5,98 @@
   - size: retorna la cantidad total de nodos del árbol
   - insert: agrega un nodo en el lugar correspondiente
   - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
-  - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
+  //- depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, 
+  //según se indique por parámetro ("post-order", "pre-order", o "in-order"). 
+  //Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
-*/
-function BinarySearchTree() {}
+  */
+ function BinarySearchTree(value) {
+    //SIZE: RETORNAR LA CANTIDAD TOTAL DE NODOS DEL ARBOL
+    this.value = value;
+   this.left = null;
+   this.right = null;
+}
+//METODO SIZE
+
+BinarySearchTree.prototype.size = function () {
+   //quiere retornar =>1+ todo lo de la izq + todo lo de la der..
+   //contador arranca en 1 porq se cuenta a si mismo (fitipaldi)
+   var count = 1;
+   //si tiene algo a la izq arranca y suma todo hacia la izq
+   if (this.left) {
+      count += this.left.size();
+   }
+   //si tiene algo a la der arranca y suma todo hacia la der
+   if (this.right) {
+      count += this.right.size();
+   }
+
+   return count;
+}
+
+//METODO INSERT
+
+BinarySearchTree.prototype.insert = function (value) {
+
+   // 1 - me preg si es mayor o menor y en base a eso se a dnd mirar
+   if (value < this.value) {
+      //es menor =debo apuntar a la izq..
+      // 2 - me pregunto si no hay algo,  ... si no hay nada inserto el nodo nuevo
+      if (!this.left) {
+         this.left = new BinarySearchTree(value)
+         return value
+      } else { // 3 - si hay algo, le pregunto al nodo que sigue si es mayor o menor..
+         this.left.insert(value)
+      }
+   } else {
+      //es mayor
+      if (!this.right) {
+         this.right = new BinarySearchTree(value)
+         return value
+      } else {
+         this.right.insert(value)
+      }
+   }
+}
+
+
+//METODO CONTAINS 
+BinarySearchTree.prototype.contains = function (value) {
+   if (value === this.value) {
+      return true
+   } else {
+      return false
+   }
+}
+
+// METODO DEPTHFIRSTFOREACH
+
+BinarySearchTree.prototype.depthFirstForEach(cb) {
+   if (cb === "post-order") {
+      //POST ORDER : IZQ DER NODO
+      this.left.depthFirstForEach(cb);
+      this.right.depthFirstForEach(cb);
+      cb(this.value);
+   } else if (cb === "pre-order") {
+
+      //PRE ORDER : NODO IZQ DER
+      cb(this.value);
+      this.left.depthFirstForEach(cb);
+      this.right.depthFirstForEach(cb);
+   } else {
+      // IN ORDER : IZQ NODO DER
+      this.left.depthFirstForEach(cb);
+      cb(this.value);
+      this.right.depthFirstForEach(cb);
+   }
+}
+   // - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
+
+// METODO BREADTHFIRSTFOREACH
+
+BinarySearchTree.prototype.breadthFirstForEach=function(cb){
+
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
